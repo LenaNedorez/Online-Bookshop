70eca61980f3f8036ff5b1ca2dc6b67b752b9d04
Elena Nedorezova <nedorezova.emp@gmail.com>
2023-07-11 20:47:39 +0300

    change PopularController

--- src/main/java/com/example/MyBookShopApp/controllers/PopularController.java
+++ src/main/java/com/example/MyBookShopApp/controllers/PopularController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PopularController {
@@ -18,7 +17,6 @@ public class PopularController {
     }
 
     @GetMapping("/books/popular")
-    @ResponseBody
     public String popularPage(Model model) {
         model.addAttribute("popularBooks", bookService.getPopularBooks());
         return "books/popular";
